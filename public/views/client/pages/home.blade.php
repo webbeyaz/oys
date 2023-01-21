b79841dafc619e7103c0ce3a8287ad98245c44a1
eminarif <coderwax@gmail.com>
2023-01-21 11:02:42 +0300

    Update home.blade.php

--- public/views/client/pages/home.blade.php
+++ public/views/client/pages/home.blade.php
@@ -95,7 +95,7 @@
 				$.getJSON(API_URL + '/login', function (response) {
 					if (response.status === 500)
 					{
-						location.href = {{ site_url('logged') }};
+						window.location.href = '{{ site_url('logged') }}';
 					}
 				});
 			}, 2000);
