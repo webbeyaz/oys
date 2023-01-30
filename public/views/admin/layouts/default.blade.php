321dbfc7918483805cac698b704b44be01986dce
eminarif <coderwax@gmail.com>
2023-01-30 19:33:26 +0300

    Update default.blade.php

--- public/views/admin/layouts/default.blade.php
+++ public/views/admin/layouts/default.blade.php
@@ -21,14 +21,14 @@
 <!-- END: Head-->
 
 <!-- BEGIN: Body-->
-<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="">
+<body class="vertical-layout vertical-menu-modern navbar-floating footer-static" data-open="click" data-menu="vertical-menu-modern" data-col="">
 
 	@include('admin.layouts.partials.header')
 
 	@include('admin.layouts.partials.main-menu')
 
 	<!-- BEGIN: Content-->
-	<div class="app-content content ">
+	<div class="app-content content">
 		<div class="content-overlay"></div>
 		<div class="header-navbar-shadow"></div>
 		<div class="content-wrapper container-xxl p-0">
