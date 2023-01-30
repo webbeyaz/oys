0b7e775a0c023b2fda8606fe54dd1bd0d20c72ac
eminarif <coderwax@gmail.com>
2023-01-30 14:44:13 +0300

    Update app.js

--- public/assets/app/js/core/app.js
+++ public/assets/app/js/core/app.js
@@ -610,7 +610,7 @@ window.colors = {
           $pageList =
             '<li class="d-flex align-items-center">' +
             '<a href="#">' +
-            '<h6 class="section-label mt-75 mb-0">Pages</h6>' +
+            '<h6 class="section-label mt-75 mb-0">Sayfalar</h6>' +
             '</a>' +
             '</li>',
           $activeItemClass = '',
