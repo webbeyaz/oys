66b6ff07b65e7be1ed8368adcce93b2a5685cbf8
eminarif <coderwax@gmail.com>
2023-01-30 23:31:27 +0300

    Update

--- app/Controllers/Admin/Auth/Recovery.php
+++ app/Controllers/Admin/Auth/Recovery.php
@@ -4,6 +4,10 @@ namespace App\Controllers\Admin\Auth;
 
 use App\Controllers\Admin;
 use Symfony\Component\HttpFoundation\Request;
+use PDO;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class Recovery extends Admin
 {
@@ -29,8 +33,90 @@ class Recovery extends Admin
 
 				$email = $data['email'];
 
-				echo $email;
-				exit;
+				$sql = "
+					SELECT
+						id
+					FROM users
+					WHERE
+						(status = 1 AND deleted_at IS NULL)
+						AND
+						(email = '{$email}')
+				";
+
+				$query = $this->db->query($sql)->fetch(PDO::FETCH_OBJ);
+
+				if ($query)
+				{
+					$id = $query->id;
+					$token = hashid();
+
+					$sql = "
+						INSERT INTO recovery SET
+						user_id = ?,
+						token = ?
+					";
+
+					$query = $this->db->prepare($sql);
+
+					if ($query)
+					{
+						$insert = $query->execute([
+							$id,
+							$token
+						]);
+
+						if ($insert)
+						{
+							$mail = new PHPMailer(true);
+
+							try
+							{
+								$mail->SMTPDebug = false;
+								$mail->isSMTP();
+								$mail->Host = 'mail.oysapp.com';
+								$mail->SMTPAuth = true;
+								$mail->Username = 'admin@oysapp.com';
+								$mail->Password = '&zm4X!HNxel0';
+								$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+								$mail->Port = 465;
+
+								$mail->setFrom('admin@oysapp.com', 'Ofis Yönetim Sistemi');
+								$mail->addAddress($email);
+
+								$mail->isHTML();
+								$mail->Subject = 'Şifre Sıfırlama Bağlantısı';
+								$mail->Body = 'Şifre Sıfırlama Bağlantınız: ' . site_url('admin/recovery/' . $token);
+								$mail->AltBody = 'OYS';
+
+								$mail->send();
+
+								header('Location: ' . site_url('admin/recovery/sent'));
+								exit;
+							}
+							catch (Exception $e)
+							{
+								$error = [
+									'class' => 'danger',
+									'text' => 'Sistemde bir hata oluştu ve e-posta gönderilemedi.'
+								];
+							}
+						}
+					}
+					else
+					{
+						$error = [
+							'class' => 'danger',
+							'text' => 'Sistemde bir hata oluştu ve bağlantı oluşturulamadı.'
+						];
+					}
+				}
+				else
+				{
+					$error = [
+						'class' => 'danger',
+						'text' => 'Bu e-posta adresine kayıtlı bir kullanıcı bulunamadı.'
+					];
+				}
 			}
 			else
 			{
