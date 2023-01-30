1d6b06ca133981ac230ec9024ba877a3295d5fae
eminarif <coderwax@gmail.com>
2023-01-30 10:05:22 +0300

    Update

---
+++ public/views/admin/layouts/partials/styles.blade.php
@@ -0,0 +1,25 @@
+<!-- BEGIN: Vendor CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/vendors.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/charts/apexcharts.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/extensions/toastr.min.css') }}">
+<!-- END: Vendor CSS-->
+
+<!-- BEGIN: Theme CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/bootstrap.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/bootstrap-extended.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/colors.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/components.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/themes/dark-layout.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/themes/bordered-layout.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/themes/semi-dark-layout.css') }}">
+
+<!-- BEGIN: Page CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/core/menu/menu-types/vertical-menu.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/pages/dashboard-ecommerce.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/plugins/charts/chart-apex.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/plugins/extensions/ext-component-toastr.css') }}">
+<!-- END: Page CSS-->
+
+<!-- BEGIN: Custom CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset_url('css/style.css') }}">
+<!-- END: Custom CSS-->
