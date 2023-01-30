cf88172982b27f794d19cd3b25b6bc8a8bcabb38
eminarif <coderwax@gmail.com>
2023-01-30 14:12:12 +0300

    Update header.blade.php

--- public/views/admin/layouts/partials/header.blade.php
+++ public/views/admin/layouts/partials/header.blade.php
@@ -1,51 +1,6 @@
 <!-- BEGIN: Header-->
 <nav class="header-navbar navbar navbar-expand-lg align-items-center floating-nav navbar-light navbar-shadow container-xxl">
 	<div class="navbar-container d-flex content">
-		<div class="bookmark-wrapper d-flex align-items-center">
-			<ul class="nav navbar-nav d-xl-none">
-				<li class="nav-item">
-					<a class="nav-link menu-toggle" href="#">
-						<i class="ficon" data-feather="menu"></i>
-					</a>
-				</li>
-			</ul>
-			<ul class="nav navbar-nav bookmark-icons">
-				<li class="nav-item d-none d-lg-block">
-					<a class="nav-link" href="app-email.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Email">
-						<i class="ficon" data-feather="mail"></i>
-					</a>
-				</li>
-				<li class="nav-item d-none d-lg-block">
-					<a class="nav-link" href="app-chat.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Chat">
-						<i class="ficon" data-feather="message-square"></i>
-					</a>
-				</li>
-				<li class="nav-item d-none d-lg-block">
-					<a class="nav-link" href="app-calendar.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Calendar">
-						<i class="ficon" data-feather="calendar"></i>
-					</a>
-				</li>
-				<li class="nav-item d-none d-lg-block">
-					<a class="nav-link" href="app-todo.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Todo">
-						<i class="ficon" data-feather="check-square"></i>
-					</a>
-				</li>
-			</ul>
-			<ul class="nav navbar-nav">
-				<li class="nav-item d-none d-lg-block">
-					<a class="nav-link bookmark-star">
-						<i class="ficon text-warning" data-feather="star"></i>
-					</a>
-					<div class="bookmark-input search-input">
-						<div class="bookmark-input-icon">
-							<i data-feather="search"></i>
-						</div>
-						<input type="text" name="q" placeholder="Yer imleri" class="form-control input" tabindex="0" data-search="search">
-						<ul class="search-list search-list-bookmark"></ul>
-					</div>
-				</li>
-			</ul>
-		</div>
 		<ul class="nav navbar-nav align-items-center ms-auto">
 			<li class="nav-item dropdown dropdown-language">
 				<a href="#" class="nav-link dropdown-toggle" id="dropdown-flag" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
