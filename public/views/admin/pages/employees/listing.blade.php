7ddc02a8f01c3d3cad71d64d29f3f45c4bf734a4
eminarif <coderwax@gmail.com>
2023-02-01 01:25:20 +0300

    Update listing.blade.php

--- public/views/admin/pages/employees/listing.blade.php
+++ public/views/admin/pages/employees/listing.blade.php
@@ -3,16 +3,11 @@
 @section('title', 'Ofis Yönetim Sistemi')
 
 @section('styles_vendor')
-	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/vendors.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/tables/datatable/dataTables.bootstrap5.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/tables/datatable/responsive.bootstrap5.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/tables/datatable/buttons.bootstrap5.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/tables/datatable/rowGroup.bootstrap5.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/pickers/flatpickr/flatpickr.min.css') }}">
+
 @endsection
 
 @section('styles_page')
-	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/core/menu/menu-types/vertical-menu.css') }}">
+
 @endsection
 
 @section('styles')
@@ -24,14 +19,21 @@
 		<div class="content-header-left col-md-9 col-12 mb-2">
 			<div class="row breadcrumbs-top">
 				<div class="col-12">
-					<h2 class="content-header-title float-start mb-0">DataTables</h2>
+					<h2 class="content-header-title float-start mb-0">
+						Personel Listesi
+					</h2>
 					<div class="breadcrumb-wrapper">
 						<ol class="breadcrumb">
-							<li class="breadcrumb-item"><a href="index.html">Home</a>
+							<li class="breadcrumb-item">
+								<a href="{{ site_url('admin/dashboard') }}">
+									Anasayfa
+								</a>
 							</li>
-							<li class="breadcrumb-item"><a href="#">Datatable</a>
+							<li class="breadcrumb-item">
+								Personeller
 							</li>
-							<li class="breadcrumb-item active">Basic
+							<li class="breadcrumb-item active">
+								Personel Listesi
 							</li>
 						</ol>
 					</div>
@@ -40,103 +42,98 @@
 		</div>
 		<div class="content-header-right text-md-end col-md-3 col-12 d-md-block d-none">
 			<div class="mb-1 breadcrumb-right">
-				<div class="dropdown">
-					<button class="btn-icon btn btn-primary btn-round btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="grid"></i></button>
-					<div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item" href="app-todo.html"><i class="me-1" data-feather="check-square"></i><span class="align-middle">Todo</span></a><a class="dropdown-item" href="app-chat.html"><i class="me-1" data-feather="message-square"></i><span class="align-middle">Chat</span></a><a class="dropdown-item" href="app-email.html"><i class="me-1" data-feather="mail"></i><span class="align-middle">Email</span></a><a class="dropdown-item" href="app-calendar.html"><i class="me-1" data-feather="calendar"></i><span class="align-middle">Calendar</span></a></div>
-				</div>
+				tarih filtreleme
 			</div>
 		</div>
 	</div>
 	<div class="content-body">
-		<div class="row">
+
+		<!-- Basic Tables start -->
+		<div class="row" id="basic-table">
 			<div class="col-12">
-				<p>Read full documnetation <a href="https://datatables.net/" target="_blank">here</a></p>
-			</div>
-		</div>
-		<!-- Basic table -->
-		<section id="basic-datatable">
-			<div class="row">
-				<div class="col-12">
-					<div class="card">
-						<table class="datatables-basic table">
+				<div class="card">
+					<div class="card-header">
+						<h4 class="card-title">
+							Personeller
+						</h4>
+					</div>
+					<div class="card-body">
+						<p class="card-text">
+							Bu sayfa içerisinden personelleri özellikleriyle beraber görüntüleyebilirsiniz.
+						</p>
+					</div>
+					<div class="table-responsive">
+						<table class="table">
 							<thead>
-							<tr>
-								<th></th>
-								<th></th>
-								<th>id</th>
-								<th>Name</th>
-								<th>Email</th>
-								<th>Date</th>
-								<th>Salary</th>
-								<th>Status</th>
-								<th>Action</th>
-							</tr>
+								<tr>
+									<th>
+										Personel
+									</th>
+									<th>
+										Ad Soyad
+									</th>
+									<th>
+										Durum
+									</th>
+									<th>
+										Oluşturulma Tarihi
+									</th>
+									<th>
+										İşlem
+									</th>
+								</tr>
 							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<img src="{{ asset_url('app/images/icons/angular.svg') }}" alt="Angular" width="20" height="20" class="me-75">
+										<span class="fw-bold">Kullanıcı Adı</span>
+									</td>
+									<td>
+										Personel
+									</td>
+									<td>
+										<span class="badge rounded-pill badge-light-primary me-1">Active</span>
+									</td>
+									<td>
+										Tarih
+									</td>
+									<td>
+										<div class="dropdown">
+											<button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0" data-bs-toggle="dropdown">
+												<i data-feather="more-vertical"></i>
+											</button>
+											<div class="dropdown-menu dropdown-menu-end">
+												<a href="#" class="dropdown-item">
+													<i data-feather="edit-2" class="me-50"></i>
+													<span>Düzenle</span>
+												</a>
+												<a href="#" class="dropdown-item">
+													<i data-feather="trash" class="me-50"></i>
+													<span>Sil</span>
+												</a>
+											</div>
+										</div>
+									</td>
+								</tr>
+							</tbody>
 						</table>
 					</div>
 				</div>
 			</div>
-			<!-- Modal to add new record -->
-			<div class="modal modal-slide-in fade" id="modals-slide-in">
-				<div class="modal-dialog sidebar-sm">
-					<form class="add-new-record modal-content pt-0">
-						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
-						<div class="modal-header mb-1">
-							<h5 class="modal-title" id="exampleModalLabel">New Record</h5>
-						</div>
-						<div class="modal-body flex-grow-1">
-							<div class="mb-1">
-								<label class="form-label" for="basic-icon-default-fullname">Full Name</label>
-								<input type="text" class="form-control dt-full-name" id="basic-icon-default-fullname" placeholder="John Doe" aria-label="John Doe" />
-							</div>
-							<div class="mb-1">
-								<label class="form-label" for="basic-icon-default-post">Post</label>
-								<input type="text" id="basic-icon-default-post" class="form-control dt-post" placeholder="Web Developer" aria-label="Web Developer" />
-							</div>
-							<div class="mb-1">
-								<label class="form-label" for="basic-icon-default-email">Email</label>
-								<input type="text" id="basic-icon-default-email" class="form-control dt-email" placeholder="john.doe@example.com" aria-label="john.doe@example.com" />
-								<small class="form-text"> You can use letters, numbers & periods </small>
-							</div>
-							<div class="mb-1">
-								<label class="form-label" for="basic-icon-default-date">Joining Date</label>
-								<input type="text" class="form-control dt-date" id="basic-icon-default-date" placeholder="MM/DD/YYYY" aria-label="MM/DD/YYYY" />
-							</div>
-							<div class="mb-4">
-								<label class="form-label" for="basic-icon-default-salary">Salary</label>
-								<input type="text" id="basic-icon-default-salary" class="form-control dt-salary" placeholder="$12000" aria-label="$12000" />
-							</div>
-							<button type="button" class="btn btn-primary data-submit me-1">Submit</button>
-							<button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</section>
-		<!--/ Basic table -->
+		</div>
+		<!-- Basic Tables end -->
 
 	</div>
 
 @endsection
 
 @section('scripts_vendor')
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/jquery.dataTables.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/dataTables.bootstrap5.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/dataTables.responsive.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/responsive.bootstrap5.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/datatables.checkboxes.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/datatables.buttons.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/jszip.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/pdfmake.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/vfs_fonts.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/buttons.html5.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/buttons.print.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/tables/datatable/dataTables.rowGroup.min.js') }}"></script>
-	<script src="{{ asset_url('app/vendors/js/pickers/flatpickr/flatpickr.min.js') }}"></script>
+
 @endsection
 
 @section('scripts_page')
-	<script src="{{ asset_url('app/js/scripts/tables/table-datatables-basic.js') }}"></script>
+
 @endsection
 
 @section('scripts')
