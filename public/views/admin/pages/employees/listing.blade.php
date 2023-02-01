dda75139e8a1071d55656f5f96371ade06eefbba
eminarif <coderwax@gmail.com>
2023-02-01 15:31:18 +0300

    Update listing.blade.php

--- public/views/admin/pages/employees/listing.blade.php
+++ public/views/admin/pages/employees/listing.blade.php
@@ -3,11 +3,15 @@
 @section('title', 'Ofis Yönetim Sistemi')
 
 @section('styles_vendor')
-
+	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/tables/datatable/dataTables.bootstrap5.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/tables/datatable/responsive.bootstrap5.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/tables/datatable/buttons.bootstrap5.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/tables/datatable/rowGroup.bootstrap5.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/vendors/css/pickers/flatpickr/flatpickr.min.css') }}">
 @endsection
 
 @section('styles_page')
-
+	<link rel="stylesheet" type="text/css" href="{{ asset_url('app/css/core/menu/menu-types/vertical-menu.css') }}">
 @endsection
 
 @section('styles')
@@ -48,48 +52,12 @@
 	</div>
 	<div class="content-body">
 
-		<!-- Basic Tables start -->
-		<div class="row" id="basic-table">
-			<div class="col-12">
-				<div class="card">
-					<div class="card-header border-bottom p-1">
-						<div class="card-title">
-							Personeller
-						</div>
-						<div class="text-end">
-							<div class="d-inline-flex">
-								<div class="btn-group">
-									<button class="btn btn-outline-secondary dropdown-toggle" type="button" id="exportMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-										<span>
-											<i data-feather="share" class="font-small-4 me-50"></i>
-											Dışarı Aktar
-										</span>
-									</button>
-									<div class="dropdown-menu" aria-labelledby="exportMenuButton">
-										<a href="#" class="dropdown-item">
-											<span>
-												<i data-feather="printer" class="font-small-4 me-50"></i>
-											</span>
-											Yazdır
-										</a>
-										<a href="#" class="dropdown-item">
-											<span>
-												<i data-feather="file-text" class="font-small-4 me-50"></i>
-											</span>
-											CSV
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="card-body">
-						<p class="card-text">
-							Bu sayfa içerisinden personelleri özellikleriyle beraber görüntüleyebilirsiniz.
-						</p>
-					</div>
-					<div class="table-responsive">
-						<table class="table">
+		<!-- Basic table -->
+		<section id="classic-datatable">
+			<div class="row">
+				<div class="col-12">
+					<div class="card">
+						<table class="datatables-basic table">
 							<thead>
 								<tr>
 									<th>
@@ -155,20 +123,74 @@
 					</div>
 				</div>
 			</div>
-		</div>
-		<!-- Basic Tables end -->
+			<!-- Modal to add new record -->
+			<div class="modal modal-slide-in fade" id="modals-slide-in">
+				<div class="modal-dialog sidebar-sm">
+					<form class="add-new-record modal-content pt-0">
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
+						<div class="modal-header mb-1">
+							<h5 class="modal-title" id="exampleModalLabel">New Record</h5>
+						</div>
+						<div class="modal-body flex-grow-1">
+							<div class="mb-1">
+								<label class="form-label" for="basic-icon-default-fullname">Full Name</label>
+								<input type="text" class="form-control dt-full-name" id="basic-icon-default-fullname" placeholder="John Doe" aria-label="John Doe" />
+							</div>
+							<div class="mb-1">
+								<label class="form-label" for="basic-icon-default-post">Post</label>
+								<input type="text" id="basic-icon-default-post" class="form-control dt-post" placeholder="Web Developer" aria-label="Web Developer" />
+							</div>
+							<div class="mb-1">
+								<label class="form-label" for="basic-icon-default-email">Email</label>
+								<input type="text" id="basic-icon-default-email" class="form-control dt-email" placeholder="john.doe@example.com" aria-label="john.doe@example.com" />
+								<small class="form-text"> You can use letters, numbers & periods </small>
+							</div>
+							<div class="mb-1">
+								<label class="form-label" for="basic-icon-default-date">Joining Date</label>
+								<input type="text" class="form-control dt-date" id="basic-icon-default-date" placeholder="MM/DD/YYYY" aria-label="MM/DD/YYYY" />
+							</div>
+							<div class="mb-4">
+								<label class="form-label" for="basic-icon-default-salary">Salary</label>
+								<input type="text" id="basic-icon-default-salary" class="form-control dt-salary" placeholder="$12000" aria-label="$12000" />
+							</div>
+							<button type="button" class="btn btn-primary data-submit me-1">Submit</button>
+							<button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>
+		<!--/ Basic table -->
 
 	</div>
 
 @endsection
 
 @section('scripts_vendor')
-
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/dataTables.bootstrap5.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/dataTables.responsive.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/responsive.bootstrap5.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/datatables.checkboxes.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/datatables.buttons.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/jszip.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/pdfmake.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/vfs_fonts.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/buttons.html5.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/buttons.print.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/tables/datatable/dataTables.rowGroup.min.js') }}"></script>
+	<script src="{{ asset_url('app/vendors/js/pickers/flatpickr/flatpickr.min.js') }}"></script>
 @endsection
 
 @section('scripts_page')
-
 @endsection
 
 @section('scripts')
+
+	<script type="text/javascript">
+		$(function () {
+			$('#classic-datatable').DataTable();
+		});
+	</script>
+
 @endsection
