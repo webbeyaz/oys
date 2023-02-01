b59d10c3b512c372f68740e60c03dbcd439aaacf
eminarif <coderwax@gmail.com>
2023-02-02 00:02:00 +0300

    Update Tracking.php

--- app/Controllers/Admin/Employees/Tracking.php
+++ app/Controllers/Admin/Employees/Tracking.php
@@ -55,7 +55,7 @@ class Tracking extends Admin
 				INNER JOIN codes c ON c.id = a.code_id
 				INNER JOIN employees e ON e.id = c.employee_id
 				WHERE
-				    a.id > $action_id
+				    a.id != $action_id
 					AND
 				    e.id = $employee_id
 					AND
