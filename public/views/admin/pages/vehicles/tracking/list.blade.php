bf6860bf449bc1999bb16d329bacc5a767d9da41
eminarif <coderwax@gmail.com>
2023-02-02 17:12:37 +0300

    Update list.blade.php

--- public/views/admin/pages/vehicles/tracking/list.blade.php
+++ public/views/admin/pages/vehicles/tracking/list.blade.php
@@ -70,95 +70,95 @@
 						<div class="card">
 							<table class="datatables-basic table">
 								<thead>
-									<tr>
-										<th>
-											Şoför
-										</th>
-										<th>
-											Araç Plakası
-										</th>
-										<th>
-											Açıklama
-										</th>
-										<th>
-											Resimler
-										</th>
-										<th>
-											Oluşturulma Tarihi
-										</th>
-										<th>
-											İşlem
-										</th>
-									</tr>
+								<tr>
+									<th>
+										Şoför
+									</th>
+									<th>
+										Araç Plakası
+									</th>
+									<th>
+										Açıklama
+									</th>
+									<th>
+										Resimler
+									</th>
+									<th>
+										Oluşturulma Tarihi
+									</th>
+									<th>
+										İşlem
+									</th>
+								</tr>
 								</thead>
 								<tbody>
 
-									@foreach ($events as $event)
+								@foreach ($events as $event)
 
-										<tr>
-											<td>
-												{{ $event->firstname . ' ' . $event->lastname }}
-											</td>
-											<td>
-												{{ $event->plate }}
-											</td>
-											<td>
-												{{ $event->text }}
-											</td>
-											<td>
+									<tr>
+										<td>
+											{{ $event->firstname . ' ' . $event->lastname }}
+										</td>
+										<td>
+											{{ $event->plate }}
+										</td>
+										<td>
+											{{ $event->text }}
+										</td>
+										<!--<td>
 
-												@if ($images)
+											@if ($images)
 
-													<div class="avatar-group">
+												<div class="avatar-group">
 
-														@php
-															$i = 1;
-														@endphp
+													@foreach ($images[$event->id] as $key => $value)
 
-														@foreach ($images[$event->id] as $image)
+														<div title="{{ $key }}" class="avatar pull-up" data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="bottom">
+															<img src="{{ upload_url('images/cache/events/33x33/' . $value) }}" alt="{{ $key }}" width="33" height="33">
+														</div>
 
-															<div title="{{ $i }}" class="avatar pull-up" data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="bottom">
-																<img src="{{ upload_url('images/cache/events/33x33/' . $image) }}" alt="{{ $i }}" width="33" height="33">
-															</div>
+													@endforeach
 
-															@php
-																$i++;
-															@endphp
+													@if (count($images) > 4)
 
-														@endforeach
+														<h6 class="align-self-center cursor-pointer ms-50 mb-0">
+															+{{ count($images) - 4 }}
+														</h6>
 
-													</div>
+													@endif
 
-												@else
-
-													Resim yüklenmemiş.
-
-												@endif
-
-											</td>
-											<td>
-												{{ timeConvert($event->created_at, 'd F Y H:i') }}
-											</td>
-											<td>
-												<div class="dropdown">
-													<button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0" data-bs-toggle="dropdown">
-														<i data-feather="more-vertical"></i>
-													</button>
-													<div class="dropdown-menu dropdown-menu-end">
-														<a href="{{ site_url('admin/vehicles/tracking/edit/' . $event->id) }}" class="dropdown-item">
-															<i data-feather="edit-2" class="me-50"></i>
-															<span>Düzenle</span>
-														</a>
-														<a href="{{ site_url('admin/vehicles/tracking/delete/' . $event->id) }}" class="dropdown-item">
-															<i data-feather="trash" class="me-50"></i>
-															<span>Sil</span>
-														</a>
-													</div>
 												</div>
-											</td>
-										</tr>
 
-									@endforeach
+											@else
+
+												Resim yüklenmemiş.
+
+											@endif
+
+										</td>-->
+										<td>
+											{{ timeConvert($event->created_at, 'd F Y H:i') }}
+										</td>
+										<td>
+											<div class="dropdown">
+												<button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0" data-bs-toggle="dropdown">
+													<i data-feather="more-vertical"></i>
+												</button>
+												<div class="dropdown-menu dropdown-menu-end">
+													<a href="{{ site_url('admin/vehicles/tracking/edit/' . $event->id) }}" class="dropdown-item">
+														<i data-feather="edit-2" class="me-50"></i>
+														<span>Düzenle</span>
+													</a>
+													<a href="{{ site_url('admin/vehicles/tracking/delete/' . $event->id) }}" class="dropdown-item">
+														<i data-feather="trash" class="me-50"></i>
+														<span>Sil</span>
+													</a>
+												</div>
+											</div>
+										</td>
+									</tr>
+
+								@endforeach
 
 								</tbody>
 							</table>
