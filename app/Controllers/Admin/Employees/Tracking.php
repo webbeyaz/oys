d89797626f2bca337649beabd9710e73c10121cb
eminarif <coderwax@gmail.com>
2023-02-02 14:36:51 +0300

    Update Tracking.php

--- app/Controllers/Admin/Employees/Tracking.php
+++ app/Controllers/Admin/Employees/Tracking.php
@@ -29,61 +29,55 @@ class Tracking extends Admin
 
 		$queryIn = $this->db->query($sql, PDO::FETCH_OBJ);
 
-		if ($queryIn->rowCount())
+		$i = 0;
+
+		foreach ($queryIn as $rowIn)
 		{
-			$i = 0;
+			$action_id = $rowIn->action_id;
+			$employee_id = $rowIn->employee_id;
 
-			foreach ($queryIn as $rowIn)
+			$timeIn = $rowIn->time;
+			$explode = explode(' ', $timeIn);
+
+			$date = $explode[0];
+			$time = $explode[1];
+
+			$tracking[$i] = [
+				'username' => $rowIn->username,
+				'firstname' => $rowIn->firstname,
+				'lastname' => $rowIn->lastname,
+				'time_in' => strtotime($time) < strtotime('17:30:00') ? $timeIn : null,
+				'time_out' => strtotime($time) < strtotime('17:30:00') ? null : $timeIn,
+				'date' => $date
+			];
+
+			$sql = "
+				SELECT
+				    a.time AS time
+				FROM actions a
+				INNER JOIN codes c ON c.id = a.code_id
+				INNER JOIN employees e ON e.id = c.employee_id
+				WHERE
+				    a.id != $action_id
+					AND
+				    e.id = $employee_id
+					AND
+				    DATE(a.time) = '$date'
+			";
+
+			$queryOut = $this->db->query($sql, PDO::FETCH_OBJ);
+
+			foreach ($queryOut as $rowOut)
 			{
-				$action_id = $rowIn->action_id;
-				$employee_id = $rowIn->employee_id;
+				$tracking[$i]['time_out'] = $rowOut->time;
 
-				$timeIn = $rowIn->time;
-				$explode = explode(' ', $timeIn);
-
-				$date = $explode[0];
-				$time = $explode[1];
-
-				$tracking[$i] = [
-					'username' => $rowIn->username,
-					'firstname' => $rowIn->firstname,
-					'lastname' => $rowIn->lastname,
-					'time_in' => strtotime($time) < strtotime('17:30:00') ? $timeIn : null,
-					'time_out' => strtotime($time) < strtotime('17:30:00') ? null : $timeIn,
-					'date' => $date
-				];
-
-				$sql = "
-					SELECT
-					    a.time AS time
-					FROM actions a
-					INNER JOIN codes c ON c.id = a.code_id
-					INNER JOIN employees e ON e.id = c.employee_id
-					WHERE
-					    a.id != $action_id
-						AND
-					    e.id = $employee_id
-						AND
-					    DATE(a.time) = '$date'
-				";
-
-				$queryOut = $this->db->query($sql, PDO::FETCH_OBJ);
-
-				if ($queryOut->rowCount())
+				if ($rowOut->time < $timeIn)
 				{
-					foreach ($queryOut as $rowOut)
-					{
-						$tracking[$i]['time_out'] = $rowOut->time;
-
-						if ($rowOut->time < $timeIn)
-						{
-							unset($tracking[$i]);
-						}
-					}
+					unset($tracking[$i]);
 				}
-
-				$i++;
 			}
+
+			$i++;
 		}
 
 		$this->data['tracking'] = $tracking;
