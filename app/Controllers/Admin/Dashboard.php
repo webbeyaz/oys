a393e03b7fc381b0650f56fb287e47d9928ced7f
eminarif <coderwax@gmail.com>
2023-02-03 14:05:35 +0300

    Update

--- app/Controllers/Admin/Dashboard.php
+++ app/Controllers/Admin/Dashboard.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\Admin;
+use PDO;
 
 class Dashboard extends Admin
 {
@@ -11,6 +12,79 @@ class Dashboard extends Admin
 	 */
 	public function index(): string
 	{
+		$this->statistics();
+
 		return $this->view('admin.pages.dashboard', $this->data);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function statistics(): void
+	{
+		$statistics = [
+			'employee' => 0,
+			'driver' => 0,
+			'vehicle' => 0,
+			'event' => 0
+		];
+
+		$sql = "
+			SELECT
+				COUNT(id) AS total
+			FROM employees
+			WHERE deleted_at IS NULL
+		";
+
+		$queryEmployee = $this->db->query($sql)->fetch(PDO::FETCH_OBJ);
+
+		if ($queryEmployee)
+		{
+			$statistics['employee'] = $queryEmployee->total;
+		}
+
+		$sql = "
+			SELECT
+				COUNT(id) AS total
+			FROM drivers
+			WHERE deleted_at IS NULL
+		";
+
+		$queryDriver = $this->db->query($sql)->fetch(PDO::FETCH_OBJ);
+
+		if ($queryDriver)
+		{
+			$statistics['driver'] = $queryDriver->total;
+		}
+
+		$sql = "
+			SELECT
+				COUNT(id) AS total
+			FROM vehicles
+			WHERE deleted_at IS NULL
+		";
+
+		$queryVehicle = $this->db->query($sql)->fetch(PDO::FETCH_OBJ);
+
+		if ($queryVehicle)
+		{
+			$statistics['vehicle'] = $queryVehicle->total;
+		}
+
+		$sql = "
+			SELECT
+				COUNT(id) AS total
+			FROM events
+			WHERE deleted_at IS NULL
+		";
+
+		$queryEvent = $this->db->query($sql)->fetch(PDO::FETCH_OBJ);
+
+		if ($queryEvent)
+		{
+			$statistics['event'] = $queryEvent->total;
+		}
+
+		$this->data['statistics'] = $statistics;
 	}
 }
--- public/views/admin/pages/dashboard.blade.php
+++ public/views/admin/pages/dashboard.blade.php
@@ -27,7 +27,7 @@
 			<div class="row match-height">
 
 				<!-- Medal Card -->
-				<div class="col-xl-4 col-md-6 col-12">
+				<!--<div class="col-xl-4 col-md-6 col-12">
 					<div class="card card-congratulation-medal">
 						<div class="card-body">
 							<h5>
@@ -45,16 +45,20 @@
 							<img src="{{ asset_url('app/images/illustration/badge.svg') }}" alt="Madalya"  class="congratulation-medal">
 						</div>
 					</div>
-				</div>
+				</div>-->
 				<!--/ Medal Card -->
 
 				<!-- Statistics Card -->
-				<div class="col-xl-8 col-md-6 col-12">
+				<div class="col-12">
 					<div class="card card-statistics">
 						<div class="card-header">
-							<h4 class="card-title">Statistics</h4>
+							<h4 class="card-title">
+								İstatistikler
+							</h4>
 							<div class="d-flex align-items-center">
-								<p class="card-text font-small-2 me-25 mb-0">Updated 1 month ago</p>
+								<p class="card-text font-small-2 me-25 mb-0">
+									Az önce güncellendi
+								</p>
 							</div>
 						</div>
 						<div class="card-body statistics-body">
@@ -63,12 +67,16 @@
 									<div class="d-flex flex-row">
 										<div class="avatar bg-light-primary me-2">
 											<div class="avatar-content">
-												<i data-feather="trending-up" class="avatar-icon"></i>
+												<i data-feather="user" class="avatar-icon"></i>
 											</div>
 										</div>
 										<div class="my-auto">
-											<h4 class="fw-bolder mb-0">230k</h4>
-											<p class="card-text font-small-3 mb-0">Sales</p>
+											<h4 class="fw-bolder mb-0">
+												{{ $statistics['employee'] }}
+											</h4>
+											<p class="card-text font-small-3 mb-0">
+												Personel
+											</p>
 										</div>
 									</div>
 								</div>
@@ -76,12 +84,16 @@
 									<div class="d-flex flex-row">
 										<div class="avatar bg-light-info me-2">
 											<div class="avatar-content">
-												<i data-feather="user" class="avatar-icon"></i>
+												<i data-feather="anchor" class="avatar-icon"></i>
 											</div>
 										</div>
 										<div class="my-auto">
-											<h4 class="fw-bolder mb-0">8.549k</h4>
-											<p class="card-text font-small-3 mb-0">Customers</p>
+											<h4 class="fw-bolder mb-0">
+												{{ $statistics['driver'] }}
+											</h4>
+											<p class="card-text font-small-3 mb-0">
+												Şoför
+											</p>
 										</div>
 									</div>
 								</div>
@@ -89,25 +101,33 @@
 									<div class="d-flex flex-row">
 										<div class="avatar bg-light-danger me-2">
 											<div class="avatar-content">
-												<i data-feather="box" class="avatar-icon"></i>
+												<i data-feather="truck" class="avatar-icon"></i>
 											</div>
 										</div>
 										<div class="my-auto">
-											<h4 class="fw-bolder mb-0">1.423k</h4>
-											<p class="card-text font-small-3 mb-0">Products</p>
+											<h4 class="fw-bolder mb-0">
+												{{ $statistics['vehicle'] }}
+											</h4>
+											<p class="card-text font-small-3 mb-0">
+												Araç
+											</p>
 										</div>
 									</div>
 								</div>
-								<div class="col-xl-3 col-sm-6 col-12">
+								<div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
 									<div class="d-flex flex-row">
 										<div class="avatar bg-light-success me-2">
 											<div class="avatar-content">
-												<i data-feather="dollar-sign" class="avatar-icon"></i>
+												<i data-feather="list" class="avatar-icon"></i>
 											</div>
 										</div>
 										<div class="my-auto">
-											<h4 class="fw-bolder mb-0">$9745</h4>
-											<p class="card-text font-small-3 mb-0">Revenue</p>
+											<h4 class="fw-bolder mb-0">
+												{{ $statistics['event'] }}
+											</h4>
+											<p class="card-text font-small-3 mb-0">
+												Araç Kaydı
+											</p>
 										</div>
 									</div>
 								</div>
