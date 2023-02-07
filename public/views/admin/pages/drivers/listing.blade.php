1297ed679a02371dc7628b7f6a3774fee427f804
eminarif <coderwax@gmail.com>
2023-02-07 13:15:45 +0300

    Update listing.blade.php

--- public/views/admin/pages/drivers/listing.blade.php
+++ public/views/admin/pages/drivers/listing.blade.php
@@ -53,82 +53,98 @@
 	</div>
 	<div class="content-body">
 
-		<!-- Basic table -->
-		<section id="basic-datatable">
+		@if ($drivers)
+
+			<!-- Basic table -->
+			<section id="basic-datatable">
+				<div class="row">
+					<div class="col-12">
+						<div class="card">
+							<table class="datatables-basic table">
+								<thead>
+									<tr>
+										<th>
+											Ad Soyad
+										</th>
+										<th>
+											E-posta Adresi
+										</th>
+										<th>
+											Telefon Numarası
+										</th>
+										<th>
+											Araç Plakası
+										</th>
+										<th>
+											Oluşturulma Tarihi
+										</th>
+										<th>
+											İşlem
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+
+									@foreach ($drivers as $driver)
+
+										<tr>
+											<td>
+												{{ $driver->firstname . ' ' . $driver->lastname }}
+											</td>
+											<td>
+												{{ $driver->email }}
+											</td>
+											<td>
+												{{ $driver->phone }}
+											</td>
+											<td>
+												{{ $driver->plate }}
+											</td>
+											<td>
+												{{ timeConvert($driver->created_at, 'd F Y H:i') }}
+											</td>
+											<td>
+												<div class="dropdown">
+													<button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0" data-bs-toggle="dropdown">
+														<i data-feather="more-vertical"></i>
+													</button>
+													<div class="dropdown-menu dropdown-menu-end">
+														<a href="{{ site_url('admin/drivers/edit/' . $driver->id) }}" class="dropdown-item">
+															<i data-feather="edit-2" class="me-50"></i>
+															<span>Düzenle</span>
+														</a>
+														<a href="{{ site_url('admin/drivers/delete/' . $driver->id) }}" class="dropdown-item">
+															<i data-feather="trash" class="me-50"></i>
+															<span>Sil</span>
+														</a>
+													</div>
+												</div>
+											</td>
+										</tr>
+
+									@endforeach
+
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!--/ Basic table -->
+
+		@else
+
 			<div class="row">
 				<div class="col-12">
-					<div class="card">
-						<table class="datatables-basic table">
-							<thead>
-								<tr>
-									<th>
-										Ad Soyad
-									</th>
-									<th>
-										E-posta Adresi
-									</th>
-									<th>
-										Telefon Numarası
-									</th>
-									<th>
-										Araç Plakası
-									</th>
-									<th>
-										Oluşturulma Tarihi
-									</th>
-									<th>
-										İşlem
-									</th>
-								</tr>
-							</thead>
-							<tbody>
-
-								@foreach ($drivers as $driver)
-
-									<tr>
-										<td>
-											{{ $driver->firstname . ' ' . $driver->lastname }}
-										</td>
-										<td>
-											{{ $driver->email }}
-										</td>
-										<td>
-											{{ $driver->phone }}
-										</td>
-										<td>
-											{{ $driver->plate }}
-										</td>
-										<td>
-											{{ timeConvert($driver->created_at, 'd F Y H:i') }}
-										</td>
-										<td>
-											<div class="dropdown">
-												<button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0" data-bs-toggle="dropdown">
-													<i data-feather="more-vertical"></i>
-												</button>
-												<div class="dropdown-menu dropdown-menu-end">
-													<a href="{{ site_url('admin/drivers/edit/' . $driver->id) }}" class="dropdown-item">
-														<i data-feather="edit-2" class="me-50"></i>
-														<span>Düzenle</span>
-													</a>
-													<a href="{{ site_url('admin/drivers/delete/' . $driver->id) }}" class="dropdown-item">
-														<i data-feather="trash" class="me-50"></i>
-														<span>Sil</span>
-													</a>
-												</div>
-											</div>
-										</td>
-									</tr>
-
-								@endforeach
-
-							</tbody>
-						</table>
+					<div class="alert alert-info" role="alert">
+						<div class="alert-body">
+							Sistemde kayıtlı araç bulunamadı.
+						</div>
 					</div>
 				</div>
 			</div>
-		</section>
-		<!--/ Basic table -->
+
+		@endif
 
 	</div>
 
