486b0f35c001e72a91bcb588f66eceeaa752ea73
eminarif <coderwax@gmail.com>
2023-02-07 13:08:05 +0300

    Update dashboard.blade.php

--- public/views/admin/pages/dashboard.blade.php
+++ public/views/admin/pages/dashboard.blade.php
@@ -80,6 +80,23 @@
 										</div>
 									</div>
 								</div>
+								<div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
+									<div class="d-flex flex-row">
+										<div class="avatar bg-light-warning me-2">
+											<div class="avatar-content">
+												<i data-feather="user-check" class="avatar-icon"></i>
+											</div>
+										</div>
+										<div class="my-auto">
+											<h4 class="fw-bolder mb-0">
+												{{ $statistics['action'] }}
+											</h4>
+											<p class="card-text font-small-3 mb-0">
+												Personel Takibi
+											</p>
+										</div>
+									</div>
+								</div>
 								<div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
 									<div class="d-flex flex-row">
 										<div class="avatar bg-light-info me-2">
