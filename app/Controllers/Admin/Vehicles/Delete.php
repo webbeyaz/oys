5b511439bd8406840f404d3d38c92887c4a360d2
eminarif <coderwax@gmail.com>
2023-02-27 12:47:09 +0300

    Update Delete.php

--- app/Controllers/Admin/Vehicles/Delete.php
+++ app/Controllers/Admin/Vehicles/Delete.php
@@ -31,10 +31,33 @@ class Delete extends Admin
 
 		if ($update)
 		{
-			$message = [
-				'class' => 'success',
-				'text' => 'Araç silme işlemi başarılı ile gerçekleşti.'
-			];
+			$sql = "
+				UPDATE drivers SET
+				vehicle_id = :vehicle_id
+				WHERE vehicle_id = :id
+			";
+
+			$query = $this->db->prepare($sql);
+
+			$update = $query->execute([
+				'vehicle_id' => NULL,
+				'id' => $id
+			]);
+
+			if ($update)
+			{
+				$message = [
+					'class' => 'success',
+					'text' => 'Araç silme işlemi başarılı ile gerçekleşti.'
+				];
+			}
+			else
+			{
+				$message = [
+					'class' => 'danger',
+					'text' => 'Sistemde bir hata oluştu ve araç mevcut şoförden silenemedi.'
+				];
+			}
 		}
 		else
 		{
